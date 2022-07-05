31ba1ef373b31f2c6363e9529cd07a4e8ea1989f
akksharvan <akksharvan@gmail.com>
2022-07-04 22:21:12 -0400

    N/A

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import request, escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations, it's a web app!"
+    zip_code = str(escape(request.args.get("zip_code", "")))
+    html = """<form action = "" method = get> 
+                    <input type = "text" name = "zip_code">
+                    <input type = "submit" value = "Display">
+                </form>"""
+    return html + zip_code
 
 
 if __name__ == "__main__":
