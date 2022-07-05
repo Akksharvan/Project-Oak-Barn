12ae373204eafb9d370d8e41c958c15db902fd31
akksharvan <akksharvan@gmail.com>
2022-07-04 20:35:21 -0400

    N/A

--- Data Analysis.py
+++ Data Analysis.py
@@ -1,44 +1,44 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import linear_model
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from sklearn import linear_model
 
-# Function Used for Cleaner Code
-def convertTimeColumnToDays(dataframe, column_ID):
-    temp_list = []
+# # Function Used for Cleaner Code
+# def convertTimeColumnToDays(dataframe, column_ID):
+#     temp_list = []
 
-    for index, value in dataframe[column_ID].items():
-        temp_list.append(value.days)
+#     for index, value in dataframe[column_ID].items():
+#         temp_list.append(value.days)
 
 
-    temp_series = pd.Series(temp_list)
-    dataframe[column_ID] = temp_series
+#     temp_series = pd.Series(temp_list)
+#     dataframe[column_ID] = temp_series
 
-# Function Used to Create List of Comparable Homes from DataFrame
-def comparable_homes(dataframe, address):
-    comparable_homes_df = dataframe
+# # Function Used to Create List of Comparable Homes from DataFrame
+# def comparable_homes(dataframe, address):
+#     comparable_homes_df = dataframe
     
-    return comparable_homes_df
+#     return comparable_homes_df
 
-# Initializing Primary DataFrame From .csv File
-df_cols = ["Sold Price"]
-df = pd.read_csv("Real Estate Data.csv", parse_dates = ["Closing Date"])
+# # Initializing Primary DataFrame From .csv File
+# df_cols = ["Sold Price"]
+# df = pd.read_csv("Real Estate Data.csv", parse_dates = ["Closing Date"])
 
-# Changing Closing Date Column to Days Since 1950
-timeSeries = df["Closing Date"] - pd.Timestamp(1950, 1, 1)
-df["Days Since 1950"] = timeSeries
-convertTimeColumnToDays(df, "Days Since 1950")
-df.drop(columns = "Closing Date")
+# # Changing Closing Date Column to Days Since 1950
+# timeSeries = df["Closing Date"] - pd.Timestamp(1950, 1, 1)
+# df["Days Since 1950"] = timeSeries
+# convertTimeColumnToDays(df, "Days Since 1950")
+# df.drop(columns = "Closing Date")
 
-# Linear Regression
-x = df[["Days Since 1950"]]
-y = df[["Sold Price"]]
+# # Linear Regression
+# x = df[["Days Since 1950"]]
+# y = df[["Sold Price"]]
 
-reg = linear_model.LinearRegression()
-reg.fit(x, y)
+# reg = linear_model.LinearRegression()
+# reg.fit(x, y)
 
 
-fig, ax = plt.subplots()
-ax.plot(df["Days Since 1950"], reg.predict(x), "g")
+# fig, ax = plt.subplots()
+# ax.plot(df["Days Since 1950"], reg.predict(x), "g")
 
-plt.show()
+# plt.show()
