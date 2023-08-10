38cddfaf02b5a5164baab32d4a181767766c9ebe
Akksharvan <akksharvan@gmail.com>
2023-08-10 12:40:13 -0400

    Edited Files

--- Version 1/models/models.py
+++ Version 1/models/models.py
@@ -1,20 +1,17 @@
-import pandas as pd
+import pandas
 from xgboost import XGBRegressor
 
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('/Users/akksharvan/workspace/Project-Oak-Barn/Version 1/data/27519.csv')
-cols_to_use = ['beds', 'full_bath', 'living_area_above_ground', 'living_area', 'year_built']
+data = pandas.read_csv('/Users/akksharvan/workspace/Project-Oak-Barn/Version 1/data/27519.csv')
+parameters = ['beds', 'full_bath', 'living_area_above_ground', 'living_area', 'year_built']
 
-X = data[cols_to_use]
+X = data[parameters]
 y = data["sold_price_per_sq_ft"]
 
 X_train, X_valid, y_train, y_valid = train_test_split(X, y)
 
-my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
-my_model.fit(X_train, y_train, 
-             early_stopping_rounds=5, 
-             eval_set=[(X_valid, y_valid)], 
-             verbose=False)
+my_model = XGBRegressor(n_estimators = 1000, learning_rate = 0.05)
+my_model.fit(X_train, y_train, early_stopping_rounds = 5, eval_set=[(X_valid, y_valid)], verbose=False)
 
-my_model.save_model('xgb_model.model')
+my_model.save_model('/Users/akksharvan/workspace/Project-Oak-Barn/Version 1/models/xgb_model.model')
